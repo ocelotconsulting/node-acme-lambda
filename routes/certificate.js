843bbf054fb6e8bcfc8604cee0522115eb3d184f
Larry Anderson <larryboymi@hotmail.com>
2016-10-05 10:49:07 -0500

    fixing save/read account file return

--- routes/certificate.js
+++ routes/certificate.js
@@ -2,11 +2,12 @@ const generateCertificate = require('../src/acme/generateCertificate')
 const isExpired = require('../src/util/isExpired')
 const config = require('../config/default.json')
 
-const runHandler = (expired) => (expired ? generateCertificate() : Promise.resolve({msg: 'Certificate is still valid, going back to bed.'}))
-
 module.exports = (req, res, next) => {
   isExpired(config['acme-domain'])
-  .then(runHandler)
+  .then((expired) =>
+    (expired
+      ? generateCertificate()
+      : Promise.resolve({msg: 'Certificate is still valid, going back to bed.'})))
   .then((data) => {
     res.send(data)
   })
