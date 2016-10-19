4b1a87f21a3712e1e1b3cd712a61c0c0454083b8
Larry Anderson <larryboymi@hotmail.com>
2016-10-19 14:45:35 -0500

    fix for subdomains

--- src/aws/route53/getHostedZoneId.js
+++ src/aws/route53/getHostedZoneId.js
@@ -3,12 +3,18 @@ const getRoute53 = require('../sdk/getRoute53')
 const getDomainZone = (domain, zones) =>
   zones.HostedZones.find((zone) => zone.Name === `${domain}.`)
 
-const getHostedZoneId = (domain) =>
-  getRoute53().listHostedZones().promise()
-  .then((zones) => getDomainZone(domain, zones).Id)
+const getHostedZoneId = (domain) => {
+  const parts = domain.split('.')
+  parts.splice(0, parts.length - 2)
+  return getRoute53().listHostedZones().promise()
+  .then((zones) => {
+    console.log(`The zones are ${JSON.stringify(zones)}`)
+    return getDomainZone(parts.join('.'), zones).Id
+  })
   .catch((e) => {
     console.log(`Couldn't retrieve hosted zones from Route53`)
     throw e
   })
+}
 
 module.exports = getHostedZoneId
