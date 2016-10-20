fff8c9b1c2e54d557a6e320b380e4756ebddad87
Larry Anderson <larryboymi@hotmail.com>
2016-10-20 08:35:54 -0500

    remove console log and update doc

--- src/aws/route53/getHostedZoneId.js
+++ src/aws/route53/getHostedZoneId.js
@@ -3,16 +3,12 @@ const getRoute53 = require('../sdk/getRoute53')
 const getDomainZone = (domain, zones) =>
   zones.HostedZones.find((zone) => zone.Name === `${domain}.`)
 
-const getHostedZoneId = (domain) => {
-  return getRoute53().listHostedZones().promise()
-  .then((zones) => {
-    console.log(`The zones are ${JSON.stringify(zones)}`)
-    return getDomainZone(domain.split('.').slice(-2).join('.'), zones).Id
-  })
+const getHostedZoneId = (domain) =>
+  getRoute53().listHostedZones().promise()
+  .then((zones) => getDomainZone(domain.split('.').slice(-2).join('.'), zones).Id)
   .catch((e) => {
     console.log(`Couldn't retrieve hosted zones from Route53`)
     throw e
   })
-}
 
 module.exports = getHostedZoneId
