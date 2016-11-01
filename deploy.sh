198828fffc086e85335daef0b5e7f78d69fa5ad7
Larry Anderson <larryboymi@hotmail.com>
2016-11-01 10:40:33 -0500

    add delta for build artifact

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+
+timestamp() {
+  date +"%b-%d-%Y-%H_%M_%S"
+}
+
+dist_file=dist-`timestamp`.zip
+
 npm run clean
 echo $letsencrypt_config > ./config/default.json
 npm run dist
 echo $cf_template > cf_template.json
-echo $cf_parameters > cf_parameters.json
-aws-sdk-cli cp dist.zip s3://lambda-cloudformation-test/dist.zip
+echo $cf_parameters | sed "s/dist.zip/${dist_file}/g" > cf_parameters.json
+aws-sdk-cli cp dist.zip s3://lambda-cloudformation-test/$dist_file
 aws-sdk-cli update-stack -t cf_template.json -p cf_parameters.json letsencrypt-stack
