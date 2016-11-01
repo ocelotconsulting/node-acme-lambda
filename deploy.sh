858dba854ef11aecc4a741a799834121289571c0
Larry Anderson <larryboymi@hotmail.com>
2016-11-01 14:11:01 -0500

    different delimiter

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ npm run clean
 echo $letsencrypt_config > ./config/default.json
 npm run dist
 echo $cf_template > cf_template.json
-echo $cf_parameters | sed "s/dist.zip/${LAMBCI_REPO}\/${dist_file}/g" > cf_parameters.json
+echo $cf_parameters | sed "s|dist.zip|${LAMBCI_REPO}/${dist_file}|g" > cf_parameters.json
 aws-sdk-cli cp dist.zip s3://lambci-build-artifacts/$LAMBCI_REPO/$dist_file
 aws-sdk-cli update-stack -t cf_template.json -p cf_parameters.json letsencrypt-stack
