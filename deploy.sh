edf625471f192d869f10900af38e9c175ec9979c
Larry Anderson <larryboymi@hotmail.com>
2016-11-01 13:52:59 -0500

    create by-repo artifacts

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,10 @@ timestamp() {
 }
 
 dist_file=dist-`timestamp`.zip
-printenv
 npm run clean
 echo $letsencrypt_config > ./config/default.json
 npm run dist
 echo $cf_template > cf_template.json
 echo $cf_parameters | sed "s/dist.zip/${dist_file}/g" > cf_parameters.json
-aws-sdk-cli cp dist.zip s3://lambda-cloudformation-test/$dist_file
+aws-sdk-cli cp dist.zip s3://lambda-cloudformation-test/$LAMBCI_REPO/$dist_file
 aws-sdk-cli update-stack -t cf_template.json -p cf_parameters.json letsencrypt-stack
