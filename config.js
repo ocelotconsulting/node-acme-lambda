f17939c9856dfa6bde8ef8dcbcb3162464b314f5
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-07-30 10:38:45 -0400

    Allow changing ACME_DIRECTORY_URL from env var

--- config.js
+++ config.js
@@ -3,6 +3,9 @@ const defaultCertInfo = {
   'cert-name2': ['<third-domain-needing-certificate>', '<fourth-domain-needing-certificate>']
 }
 
+let productionDirectoryUrl = process.env.ACME_DIRECTORY_URL || 'https://acme-v01.api.letsencrypt.org'
+let stagingDirectoryUrl = process.env.ACME_DIRECTORY_URL || 'https://acme-staging.api.letsencrypt.org'
+
 module.exports = {
   's3-account-bucket': process.env.S3_ACCOUNT_BUCKET || '<your-s3-account-config-bucket>',
   's3-cert-bucket': process.env.S3_CERT_BUCKET || '<your-s3-ssl-cert-bucket>',
@@ -13,6 +16,6 @@ module.exports = {
   'acme-account-file': process.env.ACME_ACCOUNT_FILE || '<where-to-save-generated-account-registration>',
   'acme-account-email': process.env.ACME_ACCOUNT_EMAIL || '<email-of-responsible-person>',
   'acme-account-key-bits': 2048,
-  'acme-directory-url': process.env.USE_PRODUCTION ? 'https://acme-v01.api.letsencrypt.org' : 'https://acme-staging.api.letsencrypt.org',
+  'acme-directory-url': process.env.USE_PRODUCTION ? productionDirectoryUrl : stagingDirectoryUrl,
   'region': process.env.AWS_REGION || 'us-east-1'
 }
